d0cd3cb99966f90daaea292108c2b07e290eb445
NTSon <thaisonbk2020@gmail.com>
2021-05-07 22:48:14 +0700

    Update and update custome file js and css

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -6,32 +6,7 @@ $(document).on('ready', function () {
   $(function () {
     $(".nicescroll").niceScroll({ cursorcolor: "#858586" });
   });
-
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-      $('#back-to-top').fadeIn();
-    } else {
-      $('#back-to-top').fadeOut();
-    }
-  });
-  // scroll body to 0px on click
-  $('#back-to-top').click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 400);
-    return false;
-  });
-
-  $('#dataTable').DataTable();
 });
-
-
-
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
-
 
 !function ($) {
   "use strict";
