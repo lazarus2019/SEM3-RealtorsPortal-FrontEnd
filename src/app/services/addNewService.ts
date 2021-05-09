8bfd3c5ffe9189f6f60f87053e588ef4c760aca1
NTSon <thaisonbk2020@gmail.com>
2021-05-09 12:14:07 +0700

    Update addNewService

--- src/app/services/addNewService.ts
+++ src/app/services/addNewService.ts
@@ -1,1 +1,9 @@
-import ""
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+@Injectable()
+export class AddNewService implements CanActivate  {
+    canActivate() {
+ // XXXXXXX
+        return false;
+    }
+}
