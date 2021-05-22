895154fe0ed4a281e92f50bbcfc860062329e103
NTSon <thaisonbk2020@gmail.com>
2021-05-22 14:51:51 +0700

    Uppload full FAQ component + service: Update, delete, add, view.

--- src/app/area/admin/faq/faq.component.ts
+++ src/app/area/admin/faq/faq.component.ts
@@ -1,49 +1,157 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FAQAPI } from 'src/app/models/faq/faq.model';
+import { FAQService } from 'src/app/services/admin/faq/faqAPI.service';
+
+// Declare custom function
+declare var alertFunction: any;
 
 @Component({
   templateUrl: './faq.component.html'
 })
-export class FAQComponent {
-  constructor() {
-    this.loadScripts();
+export class AdminFAQComponent implements OnInit {
+
+  // Declare FAQ Component
+  listFAQ: FAQAPI[] = [];
+
+  currentFAQ: FAQAPI = new FAQAPI;
+
+  // Form content
+  formFAQGroup: FormGroup = new FormGroup({});
+  constructor(
+    // Declare form builder
+    private formBuilder: FormBuilder,
+    // Services
+    private faqService: FAQService
+  ) {
+    // this.loadScripts();
   }
 
-  // Method to dynamically load JavaScript
-  loadScripts() {
+  // // Method to dynamically load JavaScript
+  // loadScripts() {
 
-    // This array contains all the files/CDNs
-    const dynamicScripts = [
-      '../../../../assets/js/modernizr.min.js',
-      '../../../../assets/js/jquery.min.js',
-      '../../../../assets/js/moment.min.js',
+  //   // This array contains all the files/CDNs
+  //   const dynamicScripts = [
+  //     '../../../../assets/js/modernizr.min.js',
+  //     '../../../../assets/js/jquery.min.js',
+  //     '../../../../assets/js/moment.min.js',
 
-      '../../../../assets/js/popper.min.js',
-      '../../../../assets/js/bootstrap.min.js',
+  //     '../../../../assets/js/popper.min.js',
+  //     '../../../../assets/js/bootstrap.min.js',
 
-      '../../../../assets/js/detect.js',
-      '../../../../assets/js/jquery.blockUI.js',
-      '../../../../assets/js/jquery.nicescroll.js',
+  //     '../../../../assets/js/detect.js',
+  //     '../../../../assets/js/jquery.blockUI.js',
+  //     '../../../../assets/js/jquery.nicescroll.js',
 
-      '../../../../assets/js/jquery.dataTable.js',
-      '../../../../assets/js/jquery.goToTop.js',
-      '../../../../assets/plugins/datatables/datatables.min.js',
+  //     '../../../../assets/js/jquery.dataTable.js',
+  //     '../../../../assets/js/jquery.goToTop.js',
+  //     '../../../../assets/plugins/datatables/datatables.min.js',
 
-      '../../../../assets/plugins/waypoints/lib/jquery.waypoints.min.js',
+  //     '../../../../assets/plugins/waypoints/lib/jquery.waypoints.min.js',
 
-      '../../../../assets/data/data_datatables.js',
+  //     '../../../../assets/data/data_datatables.js',
 
-      '../../../../assets/plugins/jquery.filer/js/jquery.filer.min.js',
-      '../../../../assets/plugins/jquery.filer/js/temp.js',
-      '../../../../assets/plugins/sweetalert/sweetalert.min.js',
-      '../../../../assets/js/jquery.sweetalert.js'
+  //     '../../../../assets/plugins/jquery.filer/js/jquery.filer.min.js',
+  //     '../../../../assets/plugins/jquery.filer/js/temp.js',
+  //     '../../../../assets/plugins/sweetalert/sweetalert.min.js',
+  //     '../../../../assets/js/jquery.sweetalert.js'
 
-    ];
-    for (let i = 0; i < dynamicScripts.length; i++) {
-      const node = document.createElement('script');
-      node.src = dynamicScripts[i];
-      node.type = 'text/javascript';
-      node.async = false;
-      document.body.appendChild(node);
-    }
+  //   ];
+  //   for (let i = 0; i < dynamicScripts.length; i++) {
+  //     const node = document.createElement('script');
+  //     node.src = dynamicScripts[i];
+  //     node.type = 'text/javascript';
+  //     node.async = false;
+  //     document.body.appendChild(node);
+  //   }
+  // }
+
+  ngOnInit() {
+    this.formFAQGroup = this.formBuilder.group({
+      title: new FormControl("", [Validators.required, Validators.minLength(5)]),
+      description: new FormControl("", [Validators.required, Validators.minLength(20)])
+    });
+
+    // Get and show all FAQ from database
+    this.getAllFAQ();
+  }
+
+  getAllFAQ() {
+    this.faqService.getAllFAQ().then(
+      res => {
+        this.listFAQ = res;
+      },
+      err => {
+        alertFunction.error("Can not get all faq!");
+      }
+    )
+  }
+
+  viewToEditFAQ(faqId: number) {
+    this.faqService.findFAQ(faqId).then(
+      res => {
+        this.currentFAQ = res;
+        this.formFAQGroup.get("title")?.setValue(this.currentFAQ.title);
+        this.formFAQGroup.get("description")?.setValue(this.currentFAQ.description);
+      },
+      err => {
+        alertFunction.error("Can not get faq to edit!");
+      }
+    )
+  }
+
+  resetCurrentFAQ() {
+    this.currentFAQ = new FAQAPI;
+    this.formFAQGroup.get("title")?.setValue(this.currentFAQ.title);
+    this.formFAQGroup.get("description")?.setValue(this.currentFAQ.description);
+  }
+
+  addFAQ(){
+    let faq:FAQAPI = this.formFAQGroup.value;
+    this.faqService.createFAQ(faq).then(
+      res=>{
+        alertFunction.success("You just add a new FAQ!");
+        this.getAllFAQ();
+      },
+      err =>{
+        alertFunction.error("Can not add new FAQ!");        
+      }
+    )
+  }
+
+  deleteFAQ(faqId: number){
+    this.faqService.deleteFAQ(faqId).then(
+      res=>{
+        alertFunction.success("Your FAQ had been deleted!");
+        this.getAllFAQ();
+      },
+      err =>{
+        alertFunction.error("Can not delete this FAQ!");
+      }
+    )
+  }
+
+  updateFAQ(faqId:number){
+    let faq:FAQAPI = this.formFAQGroup.value;
+    faq.faqId = faqId;
+console.log(faq);
+    this.faqService.updateFAQ(faq).then(
+      res=>{
+        alertFunction.success("Your FAQ had been updated!");
+        this.getAllFAQ();
+      },
+      err=>{
+        alertFunction.error("Can not update your FAQ!");
+      }
+    )
+  }
+
+  // Alert
+  test_success_alert() {
+    alertFunction.success("new");
+  }
+
+  test_error_alert() {
+    alertFunction.error("new");
   }
 }
---
+++ src/app/services/admin/faq/faqAPI.service.ts
@@ -0,0 +1,44 @@
+import { FAQAPI } from './../../../models/faq/faq.model';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class FAQService {
+
+    private BASE_URL = 'http://localhost:65320/api/admin/faq/';
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    getAllFAQ() {
+        return this.http.get(this.BASE_URL + "getAllFAQ")
+            .toPromise()
+            .then(res => res as FAQAPI[])
+    }
+
+    findFAQ(faqId: number) {
+        return this.http.get(this.BASE_URL + "findFAQ/" + faqId)
+            .toPromise()
+            .then(res => res as FAQAPI);
+    }
+
+    createFAQ(faq: FAQAPI) {
+        return this.http.post(this.BASE_URL + "createFAQ", faq)
+            .toPromise()
+            .then(res => res);
+    }
+
+    deleteFAQ(faqId: number) {
+        return this.http.delete(this.BASE_URL + "deleteFAQ/" + faqId)
+            .toPromise()
+            .then(res => res);
+    }
+
+    updateFAQ(faq: FAQAPI){
+        return this.http.put(this.BASE_URL + "updateFAQ", faq)
+            .toPromise()
+            .then(res => res);
+    }
+}
