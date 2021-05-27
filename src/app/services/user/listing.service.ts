197f34dc2528ca30441521561d42420bb1daf3d0
Thu Nguyễn <thu951252@gmail.com>
2021-05-27 17:59:10 +0700

    creat mailbox  to db

--- src/app/services/user/listing.service.ts
+++ src/app/services/user/listing.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { CityModel } from "src/app/models/city.model";
 import { PopularLocations } from "src/app/models/popularLocation.model";
 import { PropertyModel } from "src/app/models/property.model";
+import { RegionModel } from "src/app/models/region.model";
 
 
 @Injectable()
@@ -22,10 +24,35 @@ export class ListingService {
                         .toPromise()
                         .then( res => res as PropertyModel[] ) ;
     }
+    searchPropertyListing(
+        keyword: string , 
+        categoryId : number , 
+        country : string, 
+        city : string , 
+        type : string , 
+        area : number , 
+        bed : number , 
+        room : number , 
+        price : number )  {
+
+        return this.httpClient.get(this.BASE_URL + 'searchpropertylisting/' +  keyword + '/' + categoryId + '/' + country + '/' + city + '/' + type + '/' + area + '/' + bed + '/' + room +'/' + price)
+                        .toPromise()
+                        .then( res => res as PropertyModel[] ) ;
+    }
     propertyDetail(propertyId : number) {
         return this.httpClient.get(this.BASE_URL + 'propertydetail/'+ propertyId)
                         .toPromise()
                         .then( res => res as PropertyModel) ; 
+    }
+    loadcity(countryId :string) {
+        return this.httpClient.get(this.BASE_URL + 'loadcity/' + countryId)
+                        .toPromise()
+                        .then( res => res as CityModel[]) ; 
+    }
+    loadallcity() {
+        return this.httpClient.get(this.BASE_URL + 'loadallcity')
+                        .toPromise()
+                        .then( res => res as CityModel[]) ; 
     }
     
 }
